34e4f911f904b78eef67e85b8d11cec3bcd4be7b
Dieter Baron <dillo@nih.at>
2002-10-08 01:28:48 +0000

    fill in image information XXX: inverted grayscale, bit order, and transparency not handled

--- src/im_tiff.c
+++ src/im_tiff.c
@@ -1,5 +1,5 @@
 /*
-  $NiH: im_tiff.c,v 1.5 2002/09/12 12:31:14 dillo Exp $
+  $NiH: im_tiff.c,v 1.6 2002/09/14 02:27:39 dillo Exp $
 
   im_tiff.c -- TIFF image handling
   Copyright (C) 2002 Dieter Baron
@@ -22,6 +22,9 @@
 #define NOSUPP_SCALE
 #define NOSUPP_RAW
 #include "image.h"
+
+static image_compression _cmp_tiff2img(int cmp);
+static image_cs_type _cspace_tiff2img(int cs);
 
 
 
@@ -77,8 +80,18 @@ tiff_open(char *fname)
     im->im.i.width = u32;
     TIFFGetField(tif, TIFFTAG_IMAGELENGTH, &u32);
     im->im.i.height = u32;
+    TIFFGetField(tif, TIFFTAG_PHOTOMETRIC, &u16);
+    im->im.i.cspace.type = _cspace_tiff2img(u16);
     TIFFGetField(tif, TIFFTAG_BITSPERSAMPLE, &u16);
     im->im.i.cspace.depth = u16;
+    TIFFGetField(tif, TIFFTAG_COMPRESSION, &u16);
+    im->im.i.compression = _cmp_tiff2img(u16);
+
+    if (im->im.i.cspace.type == IMAGE_CS_INDEXED) {
+	im->im.i.cspace.base_type = IMAGE_CS_RGB;
+	im->im.i.cspace.base_depth = 16;
+	im->im.i.cspace.ncol = 1<<im->im.i.cspace.depth;
+    }	
 
     im->im.oi = im->im.i;
 
@@ -108,5 +121,46 @@ tiff_read_start(image_tiff *im)
 {
     return;
 }		
+
+
+
+static image_compression
+_cmp_tiff2img(int cmp)
+{
+    /* XXX: COMPRESSION_OJPEG?, CCITT? */
+
+    switch(cmp) {
+    case COMPRESSION_LZW:
+	return IMAGE_CMP_LZW;
+    case COMPRESSION_JPEG:
+	return IMAGE_CMP_DCT;
+    case COMPRESSION_PACKBITS:
+	return IMAGE_CMP_RLE;
+    case COMPRESSION_ADOBE_DEFLATE:
+	return IMAGE_CMP_FLATE;
+    default:
+	return IMAGE_CMP_NONE;
+    }
+}
+
+
+
+static image_cs_type
+_cspace_tiff2img(int cs)
+{
+    switch(cs) {
+    case PHOTOMETRIC_MINISWHITE:
+    case PHOTOMETRIC_MINISBLACK:
+	/* XXX: handle reversed grayscale */
+	return IMAGE_CS_GRAY;
+    case PHOTOMETRIC_RGB:
+	return IMAGE_CS_RGB;
+    case PHOTOMETRIC_PALETTE:
+	return IMAGE_CS_INDEXED;
+    default:
+	return IMAGE_CS_UNKNOWN;
+    }
+}
+
 
 #endif /* USE_TIFF */
