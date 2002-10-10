7d7e08e24bc303ce7cb3ca7e2e2a417d6a00a8bb
Dieter Baron <dillo@nih.at>
2002-10-10 18:08:48 +0000

    add support for reading GIFs

--- src/im_gif.c
+++ src/im_gif.c
@@ -1,5 +1,5 @@
 /*
-  $NiH: im_gif.c,v 1.5 2002/09/12 12:31:13 dillo Exp $
+  $NiH: im_gif.c,v 1.6 2002/09/14 02:27:38 dillo Exp $
 
   im_gif.c -- GIF image handling
   Copyright (C) 2002 Dieter Baron
@@ -12,6 +12,9 @@
 
 #ifdef USE_GIF
 
+#include <errno.h>
+#include <stdio.h>
+
 #include <gif_lib.h>
 
 #define NOSUPP_CSPACE
@@ -20,6 +23,7 @@
 
 #include "exceptions.h"
 #include "image.h"
+#include "xmalloc.h"
 
 
 
@@ -27,19 +31,26 @@ struct image_gif {
     image im;
 
     GifFileType *gif;
+
+    char *pal;
+
+    int n;		/* size of one row */
+    char *p;		/* pointer to data of next row */
 };
 
 IMAGE_DECLARE(gif);
+
+static const char *_errstr(int err);
 
 
 
 void
 gif_close(image_gif *im)
 {
-    int ret;
-    
-    ret = (DGifCloseFile(im->gif) == GIF_OK) ? 0 : -1;
-    /* XXX: throw */
+    free(im->pal);
+
+    if (DGifCloseFile(im->gif) != GIF_OK)
+	throwf(EIO, "error closing: %s", _errstr(GifLastError()));
 
     image_free((image *)im);
 }
@@ -49,7 +60,21 @@ gif_close(image_gif *im)
 char *
 gif_get_palette(image_gif *im)
 {
-    return NULL;
+    char *pal;
+    int n, m;
+    
+    pal = (char *)im->gif->SColorMap->Colors;
+
+    if (im->gif->SColorMap->ColorCount != 1<<im->im.i.cspace.ncol) {
+	free(im->pal);
+	n = image_cspace_palette_size(&im->im.i.cspace);
+	m = im->gif->SColorMap->ColorCount * 3;
+	im->pal = xmalloc(n);
+	memcpy(im->pal, pal, m);
+	memset(im->pal+m, 0, n-m);
+	pal = im->pal;
+    }
+    return pal;
 }
 
 
@@ -73,11 +98,20 @@ gif_open(char *fname)
 	throw(&ex);
     }
 
+    im->gif = gif;
+    im->pal = NULL;
+
     im->im.i.width = gif->SWidth;
     im->im.i.height = gif->SHeight;
-    im->gif = gif;
-
+    im->im.i.cspace.type = IMAGE_CS_INDEXED;
+    im->im.i.cspace.depth = 8;
+    im->im.i.cspace.base_type = IMAGE_CS_RGB;
+    im->im.i.cspace.base_depth = 8;
+    im->im.i.cspace.ncol = 1<<gif->SColorResolution;
     im->im.oi = im->im.i;
+
+    /* no raw reading */
+    im->im.i.compression = IMAGE_CMP_NONE;
 
     return (image *)im;
 }
@@ -87,6 +121,8 @@ gif_open(char *fname)
 int
 gif_read(image_gif *im, char **bp)
 {
+    *bp = im->p;
+    im->p += im->n;
     return -1;
 }
 
@@ -95,6 +131,26 @@ gif_read(image_gif *im, char **bp)
 void
 gif_read_start(image_gif *im)
 {
+    /*
+     * Fear and Loathing: Thank's to the clean implementation and the
+     * superbly detailed documentation, I can't figure out how
+     * DGifGetLine() is supposed to work.  (Maybe it's supposed to
+     * give errors on all inputs, wouldn't surprise me.)  So damned be
+     * memory, slurp it all in.
+     */
+
+    if (DGifSlurp(im->gif) != GIF_OK)
+	throwf(EIO, "error reading image: %s",
+	       _errstr(GifLastError()));
+
+    if (im->gif->ImageCount != 1)
+	throws(EOPNOTSUPP, "multi-image GIFs not supported");
+
+    /* XXX: check that image uses whole screen */
+
+    im->n = image_get_row_size(im);
+    im->p = im->gif->SavedImages[0].RasterBits;
+    
     return;
 }
 
@@ -104,6 +160,55 @@ void
 gif_read_finish(image_gif *im, int abortp)
 {
     return;
+}
+
+
+
+static const char *
+_errstr(int err)
+{
+    static char b[128];
+
+    /*
+     * Fear and Loathing continues: giflib's desinger, in his infinite
+     * wisdom, provided no interface to get an error number's
+     * description, only to print it to stdout.  But then again, who
+     * in his right mind would ever want to do anything else with
+     * it?
+     */
+
+    switch (err) {
+    case D_GIF_ERR_OPEN_FAILED:
+	return "cannot open file";
+    case D_GIF_ERR_READ_FAILED:
+	return "read error";
+    case D_GIF_ERR_NOT_GIF_FILE:
+	return "not a GIF file";
+    case D_GIF_ERR_NO_SCRN_DSCR:
+	return "no screen descriptor detected";
+    case D_GIF_ERR_NO_IMAG_DSCR:
+	return "no image descriptor detected";
+    case D_GIF_ERR_NO_COLOR_MAP:
+	return "neither global nor local color map detected";
+    case D_GIF_ERR_WRONG_RECORD:
+	return "wrong record type detected";
+    case D_GIF_ERR_DATA_TOO_BIG:
+	return "too many pixels";
+    case D_GIF_ERR_NOT_ENOUGH_MEM:
+	return "out of memory";
+    case D_GIF_ERR_CLOSE_FAILED:
+	return "cannot close file";
+    case D_GIF_ERR_NOT_READABLE:
+	return "file not opened for reading";
+    case D_GIF_ERR_IMAGE_DEFECT:
+	return "image is defective, decoding aborted";
+    case D_GIF_ERR_EOF_TOO_SOON:
+	return "premature end of file";
+
+    default:
+	sprintf(b, "unknown error %d", err);
+	return b;
+    }
 }
 
 #endif /* USE_GIF */
