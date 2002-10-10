f7eadb68545c6053a99383293928aa73eab6bda9
Dieter Baron <dillo@nih.at>
2002-10-10 00:15:20 +0000

    support for converting depth 8<->16, type RGB<->GRAY, palette only atm

--- src/im_convert.c
+++ src/im_convert.c
@@ -1,5 +1,5 @@
 /*
-  $NiH: im_convert.c,v 1.6 2002/09/14 02:27:38 dillo Exp $
+  $NiH: im_convert.c,v 1.7 2002/10/09 14:46:43 dillo Exp $
 
   im_convert.c -- image conversion handling
   Copyright (C) 2002 Dieter Baron
@@ -17,6 +17,12 @@
 
 
 
+struct conv_info {
+    int sncomp, dncomp;		/* number of components */
+    int snbits, dnbits;		/* number of bits per component */
+    image_cs_type stype, dtype;	/* cspace type */
+};
+
 struct image_conv {
     image im;
 
@@ -24,11 +30,16 @@ struct image_conv {
     int mask;		/* which conversions are needed */
 
     char *pal;		/* palette */
+
+    struct conv_info ci[2];	/* conversion info (image, palette) */
 };
 
 IMAGE_DECLARE(conv);
 
-static void _depth16to8(char *pd, char *ps, int n);
+#define MAKE2(i, j)	((i)<<16 | (j))
+
+static void _convert(image_conv *im, char *pdc, char *psc, int n, int basep);
+static void _update_ci(image_conv *im);
 
 
 
@@ -45,19 +56,21 @@ conv_close(image_conv *im)
 char *
 conv_get_palette(image_conv *im)
 {
+    char *pal;
+
     if (im->im.i.cspace.type != IMAGE_CS_INDEXED)
 	return NULL;
 
-    if (im->mask & IMAGE_INF_BASE_DEPTH) {
-	/* XXX: currently only 16->8 */
+    pal = image_get_palette(im->oim);
+
+    if (im->mask & (IMAGE_INF_BASE_TYPE|IMAGE_INF_BASE_DEPTH)) {
 	free(im->pal);
 	im->pal = xmalloc(image_cspace_palette_size(&im->im.i.cspace));
-	_depth16to8(im->pal, image_get_palette(im->oim),
-		    image_cspace_palette_size(&im->im.i.cspace));
-	return im->pal;
+	_convert(im, im->pal, pal, 1<<im->im.i.cspace.depth, 1);
+	pal = im->pal;
     }
-    else
-	return image_get_palette(im->oim);
+    
+    return pal;
 }
 
 
@@ -161,10 +174,23 @@ image_convert(image *oim, int mask, const image_info *i)
 	mask = (mask&~IMAGE_INF_CSPACE) | m2;
 
 	if (mask & IMAGE_INF_CSPACE) {
-	    if ((m2 & IMAGE_INF_CSPACE) != IMAGE_INF_BASE_DEPTH
-		|| oim->i.cspace.base_depth != 16 || i->cspace.base_depth != 8)
-	    /* XXX: not yet */
-	    throwf(EOPNOTSUPP, "color space / depth conversion not supported");
+	    if ((mask & IMAGE_INF_BASE_TYPE)
+		&& ((oim->i.cspace.base_type != IMAGE_CS_GRAY
+		     && oim->i.cspace.base_type != IMAGE_CS_RGB)
+		    || (i->cspace.base_type != IMAGE_CS_GRAY
+			&& i->cspace.base_type != IMAGE_CS_RGB)))
+		throws(EOPNOTSUPP, "palette type conversion not supported");
+	    if ((mask & IMAGE_INF_BASE_DEPTH)
+		&& ((oim->i.cspace.base_depth != 16
+		     && oim->i.cspace.base_depth != 8)
+		    || (i->cspace.base_depth != 16
+			&& i->cspace.base_depth != 8)))
+		throws(EOPNOTSUPP, "palette depth conversion not supported");
+
+	    if ((mask & IMAGE_INF_CSPACE) & ~(IMAGE_INF_BASE_TYPE
+					      |IMAGE_INF_BASE_DEPTH))
+		throws(EOPNOTSUPP,
+		       "color space / depth conversion not supported");
 	}
     }
 
@@ -182,6 +208,8 @@ image_convert(image *oim, int mask, const image_info *i)
 
 	im->pal = NULL;
 
+	_update_ci(im);
+
 	return (image *)im;
     }
     else
@@ -191,15 +219,73 @@ image_convert(image *oim, int mask, const image_info *i)
 
 
 static void
-_depth16to8(char *pdc, char *psc, int n)
+_convert(image_conv *im, char *pdc, char *psc, int n, int basep)
 {
-    int i;
-    unsigned short *ps;
-    unsigned char *pd;
+    int c[4];
+    int i, j;
+    unsigned char *ps, *pd;
 
-    ps = (unsigned short *)psc;
+    ps = (unsigned char *)psc;
     pd = (unsigned char *)pdc;
 
-    for (i=0; i<n; i++)
-	pd[i] = ps[i]>>8;
+    for (i=0; i<n; i++) {
+	/* extract source colour */
+	for (j=0; j<im->ci[basep].sncomp; j++) {
+	    switch(im->ci[basep].snbits) {
+	    case 8:
+		c[j] = (*ps<<8)+*ps;
+		ps++;
+		break;
+	    case 16:
+		c[j] = *(unsigned short *)ps;
+		ps += 2;
+		break;
+	    }
+	}
+
+	/* convert */
+	switch (MAKE2(im->ci[basep].stype, im->ci[basep].dtype)) {
+	case MAKE2(IMAGE_CS_RGB, IMAGE_CS_GRAY):
+	    c[0] = (c[0]*7+c[1]*38+c[2]*19)/64;
+	    break;
+	case MAKE2(IMAGE_CS_GRAY, IMAGE_CS_RGB):
+	    c[1] = c[2] = c[0];
+	    break;
+	}
+
+	/* store destination colour */
+	for (j=0; j<im->ci[basep].dncomp; j++) {
+	    switch(im->ci[basep].dnbits) {
+	    case 8:
+		*pd = c[j]>>8;
+		pd++;
+		break;
+	    case 16:
+		*(unsigned short *)pd = c[j];
+		pd += 2;
+		break;
+	    }
+	}
+    }
+}
+
+
+
+static void
+_update_ci(image_conv *im)
+{
+    int i;
+
+    for (i=0; i<2; i++) {
+	im->ci[i].sncomp = image_cspace_components(&im->oim->i.cspace, i);
+	im->ci[i].dncomp = image_cspace_components(&im->im.i.cspace, i);
+    }
+    im->ci[0].snbits = im->oim->i.cspace.depth;
+    im->ci[0].dnbits = im->im.i.cspace.depth;
+    im->ci[1].snbits = im->oim->i.cspace.base_depth;
+    im->ci[1].dnbits = im->im.i.cspace.base_depth;
+    im->ci[0].stype = im->oim->i.cspace.type;
+    im->ci[0].dtype = im->im.i.cspace.type;
+    im->ci[1].stype = im->oim->i.cspace.base_type;
+    im->ci[1].dtype = im->im.i.cspace.base_type;
 }
