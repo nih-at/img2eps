f133e21937068129c36e73689b71f86939a50092
Dieter Baron <dillo@nih.at>
2002-10-11 00:51:56 +0000

    overhaul image info extraction

--- src/im_tiff.c
+++ src/im_tiff.c
@@ -1,5 +1,5 @@
 /*
-  $NiH: im_tiff.c,v 1.9 2002/10/09 14:46:44 dillo Exp $
+  $NiH: im_tiff.c,v 1.10 2002/10/10 10:58:40 dillo Exp $
 
   im_tiff.c -- TIFF image handling
   Copyright (C) 2002 Dieter Baron
@@ -26,13 +26,6 @@
 #include "image.h"
 #include "xmalloc.h"
 
-static image_compression _cmp_tiff2img(int cmp);
-static image_cs_type _cspace_tiff2img(int cs);
-static void _error_handler(const char* module, const char* fmt, va_list ap);
-#if 0
-static image_order _order_tiff2img(int or);
-#endif
-
 
 
 struct image_tiff {
@@ -47,6 +40,12 @@ struct image_tiff {
 };
 
 IMAGE_DECLARE(tiff);
+
+static void _error_handler(const char* module, const char* fmt, va_list ap);
+static void _get_cspace(image_tiff *im);
+#if 0
+static image_order _order_tiff2img(int or);
+#endif
 
 
 
@@ -88,8 +87,8 @@ tiff_open(char *fname)
 {
     image_tiff *im;
     TIFF *tif;
-    uint32 u32;
     uint16 u16;
+    uint32 u32;
     exception ex; 
 
     TIFFSetErrorHandler(NULL);
@@ -119,36 +118,7 @@ tiff_open(char *fname)
     im->pal = im->buf = NULL;
 
     if (catch(&ex) == 0) {
-	if (TIFFGetField(tif, TIFFTAG_IMAGEWIDTH, &u32) == 0)
-	    throws(EINVAL, "image width not defined");
-	im->im.i.width = u32;
-	if (TIFFGetField(tif, TIFFTAG_IMAGELENGTH, &u32) == 0)
-	    throws(EINVAL, "image height not defined");
-	im->im.i.height = u32;
-	if (TIFFGetField(tif, TIFFTAG_PHOTOMETRIC, &u16) == 0)
-	    throws(EINVAL, "image colour space type not defined");
-	im->im.i.cspace.type = _cspace_tiff2img(u16);
-	if (TIFFGetField(tif, TIFFTAG_BITSPERSAMPLE, &u16) == 0)
-	    throws(EINVAL, "image depth not defined");
-	im->im.i.cspace.depth = u16;
-	if (TIFFGetField(tif, TIFFTAG_COMPRESSION, &u16) == 0)
-	    im->im.i.compression = IMAGE_CMP_NONE;
-	else
-	    im->im.i.compression = _cmp_tiff2img(u16);
-#if 0
-	/* XXX: does libtiff take care of this?! */
-	if (TIFFGetField(tif, TIFFTAG_ORIENTATION, &u16) == 0) {
-	    im->im.i.order = _order_tiff2img(u16);
-	    if (im->im.i.order == IMAGE_ORD_UNKNOWN)
-		throwf(EINVAL, "unknown image order `%d'", u16);
-	}
-#endif
-
-	if (im->im.i.cspace.type == IMAGE_CS_INDEXED) {
-	    im->im.i.cspace.base_type = IMAGE_CS_RGB;
-	    im->im.i.cspace.base_depth = 16;
-	    im->im.i.cspace.ncol = 1<<im->im.i.cspace.depth;
-	}
+	_get_cspace(im);
 
 	if (image_get_row_size((image *)im) != TIFFScanlineSize(im->tif))
 	    throwf(EINVAL, "scanline size mismatch: image: %d, tiff: %d",
@@ -161,12 +131,6 @@ tiff_open(char *fname)
 	image_close(im);
 	throw(&ex);
     }
-
-    im->im.oi = im->im.i;
-
-    if (TIFFNumberOfStrips(im->tif) > 1
-	&& im->im.i.compression != IMAGE_CMP_RLE)
-	im->im.i.compression = IMAGE_CMP_NONE;
 
     return (image *)im;
 }
@@ -258,48 +222,6 @@ tiff_read_start(image_tiff *im)
 
 
 
-static image_compression
-_cmp_tiff2img(int cmp)
-{
-    /* XXX: COMPRESSION_OJPEG?, CCITT? */
-
-    switch(cmp) {
-    case COMPRESSION_LZW:
-	return IMAGE_CMP_LZW;
-    case COMPRESSION_JPEG:
-	return IMAGE_CMP_DCT;
-    case COMPRESSION_PACKBITS:
-	return IMAGE_CMP_RLE;
-    case COMPRESSION_ADOBE_DEFLATE:
-	return IMAGE_CMP_FLATE;
-    default:
-	return IMAGE_CMP_NONE;
-    }
-}
-
-
-
-static image_cs_type
-_cspace_tiff2img(int cs)
-{
-    switch(cs) {
-    case PHOTOMETRIC_MINISWHITE:
-    case PHOTOMETRIC_MINISBLACK:
-	/* XXX: handle reversed grayscale */
-	return IMAGE_CS_GRAY;
-    case PHOTOMETRIC_RGB:
-	return IMAGE_CS_RGB;
-    case PHOTOMETRIC_PALETTE:
-	return IMAGE_CS_INDEXED;
-    case PHOTOMETRIC_SEPARATED:
-	return IMAGE_CS_CMYK;
-    default:
-	return IMAGE_CS_UNKNOWN;
-    }
-}
-
-
-
 static void
 _error_handler(const char* module, const char* fmt, va_list ap)
 {
@@ -307,6 +229,126 @@ _error_handler(const char* module, const char* fmt, va_list ap)
     
     vasprintf(&s, fmt, ap);
     throws(EINVAL, s);
+}
+
+
+
+static void
+_get_cspace(image_tiff *im)
+{
+    uint32 u32;
+    uint16 u16, type, cmp, ocmp, nex, *ex;
+
+    /* size */
+
+    if (TIFFGetField(im->tif, TIFFTAG_IMAGEWIDTH, &u32) == 0)
+	throws(EINVAL, "image width not defined");
+    im->im.i.width = u32;
+    
+    if (TIFFGetField(im->tif, TIFFTAG_IMAGELENGTH, &u32) == 0)
+	throws(EINVAL, "image height not defined");
+    im->im.i.height = u32;
+
+    
+    /* depth */
+
+    if (TIFFGetField(im->tif, TIFFTAG_BITSPERSAMPLE, &u16) == 0)
+	throws(EINVAL, "image depth not defined");
+    im->im.i.cspace.depth = u16;
+
+    
+    /* orientation */
+#if 0
+    /* XXX: does libtiff take care of this?! */
+    if (TIFFGetField(im->tif, TIFFTAG_ORIENTATION, &u16) == 0) {
+	im->im.i.order = _order_tiff2img(u16);
+	if (im->im.i.order == IMAGE_ORD_UNKNOWN)
+	    throwf(EINVAL, "unknown image order `%d'", u16);
+    }
+#endif
+
+    
+    if (TIFFGetField(im->tif, TIFFTAG_PHOTOMETRIC, &type) == 0)
+	throws(EINVAL, "image colour space type not defined");
+    if (TIFFGetField(im->tif, TIFFTAG_COMPRESSION, &cmp) == 0)
+	cmp = COMPRESSION_NONE;
+
+
+    /* alpha channel */
+
+    if (TIFFGetField(im->tif, TIFFTAG_EXTRASAMPLES, &nex, &ex) == 1) {
+	if (nex > 1)
+	    throws(EOPNOTSUPP, "more than one extra sample not supported");
+	if (ex[0] == EXTRASAMPLE_UNASSALPHA) {
+	    im->im.i.cspace.transparency = IMAGE_TR_ALPHA;
+	}
+	else
+	    throwf(EINVAL, "unknown extra sample type `%d'", ex[0]);
+    }
+
+    /* colour space type */
+
+    switch(type) {
+    case PHOTOMETRIC_MINISWHITE:
+    case PHOTOMETRIC_MINISBLACK:
+	/* XXX: handle reversed grayscale */
+	type = IMAGE_CS_GRAY;
+	break;
+    case PHOTOMETRIC_RGB:
+	type = IMAGE_CS_RGB;
+	break;
+    case PHOTOMETRIC_PALETTE:
+	type = IMAGE_CS_INDEXED;
+	break;
+    case PHOTOMETRIC_SEPARATED:
+	type = IMAGE_CS_CMYK;
+	break;
+    case PHOTOMETRIC_YCBCR:
+	if (cmp == COMPRESSION_OJPEG || cmp == COMPRESSION_JPEG)
+	    type = IMAGE_CS_RGB;
+	else
+	    throwf(EINVAL, "unknown colour space type `%d'", type);
+	break;
+    default:
+	throwf(EINVAL, "unknown colour space type `%d'", type);
+    }
+    
+    if (type == IMAGE_CS_INDEXED) {
+	im->im.i.cspace.base_type = IMAGE_CS_RGB;
+	im->im.i.cspace.base_depth = 16;
+	im->im.i.cspace.ncol = 1<<im->im.i.cspace.depth;
+    }
+    im->im.i.cspace.type = type;
+
+
+    /* compression */
+
+    switch(cmp) {
+	/* XXX: CCITT? */
+    case COMPRESSION_LZW:
+	ocmp = cmp = IMAGE_CMP_LZW;
+	break;
+    case COMPRESSION_JPEG:
+    case COMPRESSION_OJPEG:
+	ocmp = cmp = IMAGE_CMP_DCT;
+	break;
+    case COMPRESSION_PACKBITS:
+	ocmp = cmp = IMAGE_CMP_RLE;
+	break;
+    case COMPRESSION_ADOBE_DEFLATE:
+    case COMPRESSION_DEFLATE:
+	ocmp = cmp = IMAGE_CMP_FLATE;
+	break;
+    default:
+	ocmp = cmp = IMAGE_CMP_NONE;
+    }
+    
+    if (TIFFNumberOfStrips(im->tif) > 1	&& cmp != IMAGE_CMP_RLE)
+	cmp = IMAGE_CMP_NONE;
+
+    im->im.oi = im->im.i;
+    im->im.i.compression = cmp;
+    im->im.oi.compression = ocmp;
 }
 
 
